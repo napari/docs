278ddfab66c1913caea50109041ca460408be61a
Grzegorz Bokota <bokota+github@gmail.com>
2026-08-28 05:45:26 +0200

    Prefetch data that are problematic and causing docs build failure (#1103)
    # References and relevant issues
    
    closes #1089
    
    # Description
    
    This PR adds a step of prefetching fragile data required during the
    build of docs. It does not prevent docs build from failing, but it
    should happen in the first minute.
    
    So it should be easier to understand why docs build fails and use less
    CI time.
    
    The next step might be to use this script to download data from an
    alternative location and put it in the expected place.

---
+++ docs/_scripts/prefetch_data.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+from nilearn import datasets
+import pooch
+
+
+def main():
+    # example surface timeseries
+    datasets.fetch_surf_nki_enhanced(n_subjects=1)
+    datasets.fetch_surf_fsaverage()
+
+    #getting_started/open_images.md
+    download_folder = Path(os.path.expanduser("~")).joinpath("Desktop", "napari-docs-build")
+    if not download_folder.is_dir():
+        download_folder.mkdir(parents=True, exist_ok=True)
+    pooch.retrieve(
+        url="https://ftp.ebi.ac.uk/biostudies/fire/S-BIAD/582/S-BIAD582/Files/01_wt_Dprotein555-TL/raw_mps/5-2b_01_wt_Dprotein555-TL_003_rawmp.tif",
+        known_hash='5b43ed0269eaa1eebf4c48079270a30e6cc40e87f20cc36c1b3d5a07c51c7b20',
+        fname='5-2b_01_wt_Dprotein555-TL_003_rawmp.tif',
+        path=download_folder
+    )
